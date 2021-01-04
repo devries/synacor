bb05a8358a445c1774923f905e31c288a77f95bc
Christopher De Vries <devries@idolstarastronomer.com>
2021-01-03 21:52:40 -0500

    fix an issue with the teleporter code

--- teleporter/main.go
+++ teleporter/main.go
@@ -5,19 +5,19 @@ import (
 )
 
 type Params struct {
-	a int
-	b int
-	c int
+	a uint16
+	b uint16
 }
 
 func main() {
-	known := make(map[Params]Params)
+	var a, b, c uint16
 
-	for c := 1; c < 32768; c++ {
-		a := 4
-		b := 1
+	for c = 1; c < 32768; c++ {
+		a = 4
+		b = 1
+		known := make(map[Params]uint16)
 
-		a, b = tpf(a, b, c, known)
+		a = tpf(a, b, c, known)
 		if c%10 == 0 {
 			fmt.Printf("%5d\n", c)
 		}
@@ -29,38 +29,25 @@ func main() {
 	}
 }
 
-func tpf(a, b, c int, known map[Params]Params) (int, int) {
-	p := Params{a, b, c}
+func tpf(a, b, c uint16, known map[Params]uint16) uint16 {
+	p := Params{a, b}
 
 	if a == 0 {
-		a = (b + 1) % 32768
-		return a, b
+		return b + 1
 	}
 
 	if retval, ok := known[p]; ok {
-		return retval.a, retval.b
+		return retval
 	}
 
 	if b == 0 {
-		a = a - 1
-		if a < 0 {
-			a += 32768
-		}
-		b = c
-		a, b = tpf(a, b, c, known)
-		return a, b
+		a = tpf(a-1, c, c, known)
+		known[p] = a
+		return a
 	}
 
-	b = b - 1
-	if b < 0 {
-		b += 32768
-	}
-	b, _ = tpf(a, b, c, known)
-	a = a - 1
-	if a < 0 {
-		a += 32768
-	}
-	a, b = tpf(a, b, c, known)
-	known[p] = Params{a, b, c}
-	return a, b
+	b = tpf(a, b-1, c, known)
+	a = tpf(a-1, b, c, known)
+	known[p] = a
+	return a
 }
